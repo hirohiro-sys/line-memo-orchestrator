31b46d5e194a76f99c6639ab65efcd3cd9c05104
hirohiro-sys <katohiro8843@icloud.com>
2026-09-09 10:34:26 +0900

    ローカルのworkersにenv設定

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,10 @@ import { Hono } from "hono";
 type Env = {
   DB: D1Database;
   MEDIA: R2Bucket;
+  LINE_CHANNEL_ID: string;
+  LINE_CHANNEL_SECRET: string;
+  SESSION_SECRET: string;
+  APP_URL: string;
 };
 
 const app = new Hono<{ Bindings: Env }>();
